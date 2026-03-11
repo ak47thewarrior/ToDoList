28c9bdbd40727371e19fffcd3750dec3ed77d84d
RAJNISH\HP <rajnishrev@outlook.com>
2026-03-11 21:48:04 +0530

    add new function

--- list.js
+++ list.js
@@ -3,8 +3,23 @@ const textDiv = document.getElementById("textContainer");
 const creatlistbtn = document.getElementById("createbtn");
 const todoInput = document.getElementById("todoinput");
 const addTodoButton = document.getElementById("addtodo");
+const newItemsButton = document.getElementById("new-items")
+const completedItemsButton = document.getElementById('completed-items')
 
 let myTodo = []
+let currentView = "New"
+
+const deleteTodo = (timestamp) =>{
+    const itemIndex = myTodo.find((item) => item.timestamp === timestamp)
+    myTodo.splice(itemIndex,1)
+    populateCards()
+}
+
+const markAsDone = (timestamp) =>{
+    const matchingItem = myTodo.find((item) => item.timestamp === timestamp)  
+    matchingItem.status = "Done"
+    populateCards()
+}
 
 
 const getTodoCard = (todo) =>{
@@ -13,11 +28,21 @@ const getTodoCard = (todo) =>{
 
     const checkbox = document.createElement("div")
     checkbox.classList.add('checkbox')
+    if(todo.status === "Done"){
+        checkbox.style.backgroundColor = 'blue'
+    }
+    checkbox.onclick = () => markAsDone(todo.timestamp)
     card.append(checkbox)
 
     const noteContainer = document.createElement("div")
     noteContainer.innerText = todo.note
     card.append(noteContainer)
+
+    const deleteTodoBtn = document.createElement("div")
+    deleteTodoBtn.innerText = "D"
+    deleteTodoBtn.onclick = () => deleteTodo(todo.timestamp)
+    card.append(deleteTodoBtn)
+    
 
     return card
 }
@@ -25,8 +50,11 @@ const getTodoCard = (todo) =>{
 const populateCards =() => {
     textDiv.innerHTML = ''
     myTodo.forEach(todo =>{
-        const card = getTodoCard(todo)
-        textDiv.append(card)
+        if(todo.status === currentView){
+            const card = getTodoCard(todo)
+            textDiv.append(card)
+        }
+
     })
 }
 
@@ -34,7 +62,8 @@ const addItem = () => {
     const inputValue = todoInput.value;
     const newTodoObj = {
         note: inputValue,
-        timestamp: new Date()
+        timestamp: new Date(),
+        status:"New"
     }
     myTodo.push(newTodoObj)
     localStorage.setItem("todo", JSON.stringify(myTodo))
@@ -43,13 +72,28 @@ const addItem = () => {
 }
 
 
+const switchView = (view) => {
+    currentView = view
+    if(view === "New"){
+        completedItemsButton.classList.remove("highlight")
+        newItemsButton.classList.add("highlight")
+    }else{
+        completedItemsButton.classList.add("highlight")
+        newItemsButton.classList.remove("highlight")
+    }
+    populateCards()
+}
+
 function main(){
     addTodoButton.addEventListener("click", addItem);
+    completedItemsButton.addEventListener('click', () => switchView("Done"))
+    newItemsButton.addEventListener('click', () => switchView("New"))
+
     const locallySavedTodos = JSON.parse(localStorage.getItem('todo'))
     myTodo = [
         ...locallySavedTodos
     ]
-    populateCards()
+    switchView("New")
 }
 main()
 
